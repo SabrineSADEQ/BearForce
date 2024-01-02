24eaed96a424d2440c463033cf30316a1277427c
Bival2 <biaz2009@live.fr>
2024-01-02 17:27:25 +0100

    platform model entities

---
+++ src/main/java/fr/isika/cda/javaee/entity/PlatformConfiguration.java
@@ -0,0 +1,50 @@
+package fr.isika.cda.javaee.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class PlatformConfiguration {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String platformName;
+	private String logoURL;
+	@OneToOne
+	@JoinColumn(name = "superAdmin_id")
+	private SuperAdmin superAdmin;
+	
+	@OneToMany
+	private List<Pack>packs = new ArrayList<>();
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getPlatformName() {
+		return platformName;
+	}
+	public void setPlatformName(String platformName) {
+		this.platformName = platformName;
+	}
+	public String getLogoURL() {
+		return logoURL;
+	}
+	public void setLogoURL(String logoURL) {
+		this.logoURL = logoURL;
+	}
+	
+	
+
+}
